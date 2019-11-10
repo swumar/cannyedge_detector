f9e1a94c7211c15a6bed5d2099df7d50f22788ec
umar <swumar@gmail.com>
2019-11-10 17:47:40 -0500

    all functions implemented

--- cannyEdgeDetector.py
+++ cannyEdgeDetector.py
@@ -1,5 +1,5 @@
 import math
-
+import os.path
 import cv2
 import numpy as np
 
@@ -40,6 +40,9 @@ def guass_blur(img):
             #print(i-kn,i+kn+1, j-km,j+km+1)
             #break
         #break
+
+    # print(img[42:49,95:102])
+    # print(cimg[45,98])
     return cimg
 
 
@@ -56,24 +59,6 @@ def sobel(gimg):
 
     #print(gximg.dtype)
 
-    for i in range(kn+kxn):
-        gximg[i,:] = -1
-        gximg[gimgn-i,:] = -1
-        gyimg[i, :] = -1
-        gyimg[gimgn-i, :] = -1
-        gmimg[i, :] = -1
-        gmimg[gimgn-i,:] = -1
-        gaimg[i, :] = -1
-        gaimg[gimgn - i, :] = -1
-    for i in range(km+kxm):
-        gximg[:,i] = -1
-        gximg[:,gimgm-i] = -1
-        gyimg[:, i] = -1
-        gyimg[:, gimgm - i] = -1
-        gmimg[:,i] = -1
-        gmimg[:,gimgm-i] = -1
-        gaimg[:, i] = -1
-        gaimg[:, gimgm - i] = -1
 
     # print(gximg[0:7, 0:7])
     # print(gyimg[0:7, 0:7])
@@ -107,17 +92,40 @@ def sobel(gimg):
         for j in range(km + kym, gimgm - km - kym + 1):
             if gximg[i,j] != 0 :
                 gaimg[i, j] = np.degrees(np.arctan((gyimg[i,j]/gximg[i,j])))
+                if gaimg[i,j] < 0:
+                    gaimg[i,j] += 360
             # elif gximg[i,j] == 0 and gyimg[i,j] > 0:
             elif gximg[i, j] == 0:
                 gaimg[i,j] = 90
 
-    # print(gaimg[178])
+    # print(gaimg[45,98])
     # print(gaimg.min())
     # print(gaimg.max())
 
     gximg = abs(gximg//4)
     gyimg = abs(gyimg//4)
-    gmimg = abs(gmimg//4)
+    gmimg = abs(gmimg//math.sqrt(32))
+    # gaimg = abs(gaimg)
+
+
+    for i in range(kn+kxn):
+        gximg[i,:] = -1
+        gximg[gimgn-i,:] = -1
+        gyimg[i, :] = -1
+        gyimg[gimgn-i, :] = -1
+        gmimg[i, :] = -1
+        gmimg[gimgn-i,:] = -1
+        gaimg[i, :] = -1
+        gaimg[gimgn - i, :] = -1
+    for i in range(km+kxm):
+        gximg[:,i] = -1
+        gximg[:,gimgm-i] = -1
+        gyimg[:, i] = -1
+        gyimg[:, gimgm - i] = -1
+        gmimg[:,i] = -1
+        gmimg[:,gimgm-i] = -1
+        gaimg[:, i] = -1
+        gaimg[:, gimgm - i] = -1
 
     return gximg,gyimg,gmimg,gaimg
 
@@ -125,8 +133,17 @@ def sobel(gimg):
     #print(gximg.min(),gyimg.min())
 
 
-def quadrant(i):
-    pass
+# def quadrant(i):
+#
+#     if (0 <= i < 22.5) or (157.5 <= i < 202.5) or (337.5 <= i <=360):
+#         return 0
+#     if (22.5 <= i < 67.5) or (202.5 <= i < 247.5):
+#         return 1
+#     if (67.5 <= i < 112.5) or (247.5 <= i < 292.5):
+#         return 2
+#     if (112.5 <= i < 157.5) or (292.5 <= i < 337.5):
+#         return 3
+
 
 
 def nonmaxima_supress(gmimg,gaimg):
@@ -134,32 +151,112 @@ def nonmaxima_supress(gmimg,gaimg):
     gmimgn = gmimg.shape[0] - 1
     gmimgm = gmimg.shape[1] - 1
     nmsimg = np.zeros_like(gmimg)
+    for i in range(kn+kxn):
+        nmsimg[i,:] = -1
+        nmsimg[gmimgn-i,:] = -1
+    for i in range(km+kxm):
+        nmsimg[:,i] = -1
+        nmsimg[:,gmimgm-i] = -1
 
-    # for i in range(kn+kyn, gmimgn - kn-kyn+1):
-    #     for j in range(km+kym, gmimgm - km-kym+1):
-    #         i = quadrant(gaimg[i,j])
-    #         if i == 0 and gmimg[]
-    #         nmsimg[i, j] =
 
+    for i in range(kn+kyn, gmimgn - kn-kyn+1):
+        for j in range(km+kym, gmimgm - km-kym+1):
+            # i = quadrant((gaimg[i,j]))
+            # print(gaimg[i,j])
+            # break
+            # if i == 0 and (gmimg[i,j] > (gmimg[i,j-1] and gmimg[i,j+1])):
+            #     nmsimg[i, j] = gmimg[i,j]
+            # elif i == 1 and (gmimg[i,j] > (gmimg[i-1,j+1] and gmimg[i+1,j-1])):
+            #     nmsimg[i, j] = gmimg[i, j]
+            # elif i == 2 and (gmimg[i,j] > (gmimg[i-1,j] and gmimg[i+1,j])):
+            #     nmsimg[i, j] = gmimg[i, j]
+            # elif i == 3 and (gmimg[i,j] > (gmimg[i-1,j-1] and gmimg[i+1,j+1])):
+            #     nmsimg[i, j] = gmimg[i, j]
+            # else:
+            #     nmsimg[i, j] = 0
+            if (0 <= gaimg[i,j] < 22.5) or (157.5 <= gaimg[i,j] <= 202.5) or (337.5 <= gaimg[i,j] <=360):
+                x = gmimg[i,j-1]
+                y = gmimg[i,j+1]
+            elif (22.5 <= gaimg[i,j] < 67.5) or (202.5 <= i < 247.5):
+                x = gmimg[i-1,j+1]
+                y = gmimg[i+1,j-1]
+            elif (67.5 <= gaimg[i,j] < 112.5) or (247.5 <= i < 292.5):
+                x = gmimg[i-1,j]
+                y = gmimg[i+1,j]
+            elif (112.5 <= gaimg[i,j] < 157.5) or (292.5 <= i < 337.5):
+                x = gmimg[i-1,j-1]
+                y = gmimg[i+1,j+1]
+            if (gmimg[i,j] >= x) and (gmimg[i,j] >= y):
+                nmsimg[i,j] = gmimg[i,j]
+            else:
+                nmsimg[i,j] = 0
+
+    return nmsimg
+
+
+def dbl_thresh(nmsimg,gaimg,t1,t2):
+
+    nmsimgn = nmsimg.shape[0] - 1
+    nmsimgm = nmsimg.shape[1] - 1
+    dtimg = np.zeros_like(nmsimg)
+
+    for i in range(kn + kxn):
+        dtimg[i, :] = -1
+        dtimg[nmsimgn - i, :] = -1
+    for i in range(km + kxm):
+        dtimg[:, i] = -1
+        dtimg[:, nmsimgm - i] = -1
+
+    for i in range(kn + kyn, nmsimgn - kn - kyn + 1):
+        for j in range(km + kym, nmsimgm - km - kym + 1):
+            if nmsimg[i,j] < t1:
+                dtimg[i,j] = 0
+            elif nmsimg[i,j] > t2:
+                dtimg[i,j] = 255
+            elif t1 <= nmsimg[i,j] <= t2:
+                # if nmsimg[i,j-1] > t2 and abs(gaimg[i,j] - gaimg[i,j-1]) <= 45
+                for x in (-1,0,1):
+                    for y in (-1,0,1):
+                        if not (x == 0 and y == 0):
+                            if nmsimg[i+x,i+y] > t2 and abs(gaimg[i+x,i+y] - gaimg[i,j] <= 45):
+                                dtimg[i,j] = 255
+                            else:
+                                dtimg[i,j] = 0
+
+    return dtimg
 
 if __name__ == "__main__":
 
-    #imgpath = input("Enter the path to the image:")
-    #img = cv2.imread(imgpath,0)
-    # img = cv2.imread('/Users/umar/Documents/courses/cv_6643/project1/Houses-225.bmp',0)
-    img = cv2.imread('/Users/umar/Documents/courses/cv_6643/project1/Zebra-crossing-1.bmp', 0)
-    gimg = guass_blur(img)
-    #print(gimg)
-    """cv2.imshow('guass', gimg)
-    k = cv2.waitKey(0)
-    if k == 27:
-        cv2.destroyAllWindows()
-    elif k == ord('s'):"""
-    cv2.imwrite('gs_img.png',gimg)
-    gximg,gyimg,gmimg,gaimg = sobel(gimg)
-    cv2.imwrite('gx_img.png', gximg)
-    cv2.imwrite('gy_img.png', gyimg)
-    cv2.imwrite('gm_img.png', gmimg)
-    cv2.imwrite('ga_img.png', gaimg)
+    imgpath = input("Enter the path to the image:")
+    if not(os.path.exists(imgpath)):
+        print("Image path is incorrect")
+    else:
+        img = cv2.imread(imgpath,0)
+        # img = cv2.imread('/Users/umar/Documents/courses/cv_6643/project1/Houses-225.bmp',0)
+        # img = cv2.imread('/Users/umar/Documents/courses/cv_6643/project1/Zebra-crossing-1.bmp', 0)
+        gimg = guass_blur(img)                  #function call to guassian blur
+        cv2.imwrite('gs_img.png',gimg)
+        # print(gimg[])
 
-    # nmsimg = nonmaxima_supress(gmimg,gaimg)
+        gximg,gyimg,gmimg,gaimg = sobel(gimg)
+        cv2.imwrite('gx_img.png', gximg)
+        cv2.imwrite('gy_img.png', gyimg)
+        cv2.imwrite('gm_img.png', gmimg)
+        cv2.imwrite('ga_img.png', gaimg)
+
+        # print(gmimg.max(),gmimg.min())
+
+
+        nmsimg = nonmaxima_supress(gmimg,gaimg)
+        cv2.imwrite('nms_img.png',nmsimg)
+        # print(nmsimg[185,])
+
+        # print(nmsimg.max(),nmsimg.min())
+        t1 = 14
+        t2 = 28
+
+        dtimg = dbl_thresh(nmsimg,gaimg,t1,t2)
+        cv2.imwrite('dt_img.png',dtimg)
+
+
+
